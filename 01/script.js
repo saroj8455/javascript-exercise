ec9cfab5a2b10e6d740aef2142c2a4d9d4d5edc0
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-11-14 11:36:02 +0530

    update script

--- 01/script.js
+++ 01/script.js
@@ -7,3 +7,20 @@ let square = {
 
 square.side = 5
 console.log(square.area)
+
+// run `node index.js` in the terminal
+
+let hi = {
+  name: 'Hi Team',
+  get _name() {
+    return this.name;
+  },
+};
+
+console.log(hi._name);
+
+const emptyCheck = (inputArray) => {
+  console.log(inputArray);
+};
+
+emptyCheck([1, 2, 3, 45, 6]);
